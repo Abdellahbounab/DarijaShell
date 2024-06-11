6244e0bc4b0a5b83e3d06e72b4736dd52a27bcc2
Abdelaziz Chakkaf <achakkaf@c1r8p7.1337.ma>
2024-06-11 15:22:26 +0100

    leaks problem

--- main.c
+++ main.c
@@ -36,15 +36,8 @@ int main()
 	int status;
 	t_env *env;
 
-	// int i;
-	// int j;
-	// t_cmd *cmd_tmp;
-	// t_cmd *cmd_tmp2;
-	// t_file *tmp;
-
 	status = 0;
 	env = NULL;
-
 	while (1)
 	{
 		line = readline("minishell-$ ");
@@ -52,37 +45,9 @@ int main()
 		if (line[0] == 'e' && line[1] == 'x' && line[2] == 'i' && line[3] == 't')
 			exit(0);
 		command = parsing(line, env, &status);
-
-		// printf("\n");
-		// while (cmd_tmp)
-		// {
-		// 	while (cmd_tmp && cmd_tmp->files)
-		// 	{
-		// 		j = 0;
-		// 		while (cmd_tmp->files->name && cmd_tmp->files->name[j])
-		// 			free(cmd_tmp->files->name[j++]);
-		// 		if (cmd_tmp->files->name)
-		// 			free(cmd_tmp->files->name[j]);
-		// 		free(cmd_tmp->files->name);
-		// 		cmd_tmp->files->name = NULL;
-		// 		tmp = cmd_tmp->files->next;
-		// 		free(cmd_tmp->files);
-		// 		cmd_tmp->files = NULL;
-		// 		cmd_tmp->files = tmp;
-		// 	}
-		// 	i = 0;
-		// 	while (cmd_tmp && cmd_tmp->args && cmd_tmp->args[i])
-		// 		free(cmd_tmp->args[i++]);
-		// 	free(cmd_tmp->args);
-		// 	cmd_tmp2 = cmd_tmp->next;
-		// 	free(cmd_tmp);
-		// 	cmd_tmp = NULL;
-		// 	cmd_tmp = cmd_tmp2;
-		// }
-		// free(cmd_tmp);
-		print_cmd(command);
-		free_cmd(command);
-		system("leaks minishell");
+		// print_cmd(command);
+		// free_cmd(command);
+		// system("leaks minishell");
 	}
 }
 
