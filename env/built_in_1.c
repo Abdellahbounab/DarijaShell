cd6082d551236254ab239bf23329e5ad70051aa2
Abdellah Bounab <abounab@c1r5p4.1337.ma>
2024-07-17 21:32:27 +0100

    upadting special_builtins exit status

--- env/built_in_1.c
+++ env/built_in_1.c
@@ -6,7 +6,7 @@
 /*   By: abounab <abounab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 19:16:19 by abounab           #+#    #+#             */
-/*   Updated: 2024/07/16 17:52:16 by abounab          ###   ########.fr       */
+/*   Updated: 2024/07/17 21:25:57 by abounab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,19 +56,23 @@ int	builtin_pwd(t_env **env)
 int	builtin_unset(t_env **env, t_excute *cmds)
 {
 	int	i;
+	int	err;
 
 	i = 0;
+	err = 0;
 	while (cmds->arguments && cmds->arguments[i])
 	{
 		if (!cmds->arguments[i] || check_name(cmds->arguments[i]) < 0)
 		{
 			write(STDERR_FILENO, "unset : not a valid identifier\n",
 					31), g_status = 1, 0;
+			err = 1;
 			i++;
 			continue;
 		}
 		env_unset(env, cmds->arguments[i++]);
 	}
+	g_status = err;
 	return (1);
 }
 
